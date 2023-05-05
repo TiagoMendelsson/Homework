4afab9c4e81ee4cae9da60c9618edd7ef128002a
TiagoMendelsson <tiago.passos@indt.org.br>
2023-05-05 17:22:01 -0400

    add works

--- tarefa 1/AMRConsumer.js
+++ tarefa 1/AMRConsumer.js
@@ -2,6 +2,43 @@ const AMRService = require ('./AMRService');
 
 module.export = class AMRConsumer {
     
-    static async 
+    static async SaveAMR (req, res) {
+        try {
+            const {id, name, battery, status} = req.body
+            const SavedAMR = await AMRService.save(id,name,battery,status)
+            res.status(200).json(SavedAMR)
+        } catch (err) {
+            res.status(500).send({message:`${err.message}`})
+        } 
+    }
+
+    static async DeleteAMR (req, res) {
+        try {
+            const {id} = req.body
+            const DeleteAMR = await AMRService.delete (id)
+            res.status(200).json(DeleteAMR)
+        } catch (err) {
+            res.status(500).send({message:`${err.message}`})
+        }
+    }
+
+    static async updateAMR (req,res) {
+        try { 
+            const id = req.params.id
+            const {name, battery, status} = req.body
+            const UpdatedAMR = await AMRService.update(id,name,battery,status)
+            res.status(200).json(UpdatedAMR)
+        } catch ( err) {
+            res.status(500).send({message:`${err.message}`})
+        }
+    }
     
+    static async listAMR (res) {
+        try { 
+            const AMR = await AMRService.list()
+            res.status(200).json(AMR)
+        } catch (err) {
+            res.status(500).send({message:`${err.message}`})
+        }
+    }
 }
